3e6664dca58eeee528b7cc6601879402167456a0
iosefpando <iosefpandori@gmail.com>
2026-08-11 22:23:01 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'piletas-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activación
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Responder desde la caché cuando no hay internet
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
